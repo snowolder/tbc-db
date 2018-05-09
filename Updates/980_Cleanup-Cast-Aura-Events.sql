270d305693507082814522e5d1e7c8b4b7d34883
anonxs <anonxs@web.de>
2018-05-09 20:53:32 +0200

    [ACID] Devotion Aura (Rank 3) 643

---
+++ Updates/980_Cleanup-Cast-Aura-Events.sql
@@ -0,0 +1,12 @@
+-- Cleanup Cast Aura Events
+
+-- Devotion Aura (Rank 3) 643
+DELETE FROM `creature_addon` WHERE `guid` IN (9771,9784,9785);
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` IN (1038,1036,2305,2344) AND `guid` NOT IN (15864,15865));
+DELETE FROM `creature_template_addon` WHERE `entry` IN (1038,1036,2305,2344);
+INSERT INTO `creature_template_addon` (`entry`, `b2_0_sheath`, `b2_1_flags`, `auras`) VALUES
+(1038, 1, 16, ''),
+(1036, 1, 16, '643'),
+(2305, 1, 16, '643'),
+(2344, 1, 16, '643');
+
