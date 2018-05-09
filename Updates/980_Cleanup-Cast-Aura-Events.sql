493e688d00c439c417874841e47e89697070ee39
anonxs <anonxs@web.de>
2018-05-09 20:59:13 +0200

    [ACID] Stealth (Rank 2) 1785

--- Updates/980_Cleanup-Cast-Aura-Events.sql
+++ Updates/980_Cleanup-Cast-Aura-Events.sql
@@ -10,3 +10,9 @@ INSERT INTO `creature_template_addon` (`entry`, `b2_0_sheath`, `b2_1_flags`, `au
 (2305, 1, 16, '643'),
 (2344, 1, 16, '643');
 
+-- Stealth (Rank 2) 1785
+DELETE FROM `creature_template_addon` WHERE `entry` IN (768,11792);
+INSERT INTO `creature_template_addon` (`entry`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `auras`) VALUES
+(768, 33554432, 1, 16, '1785'),
+(11792, 33554432, 1, 16, '1785');
+
